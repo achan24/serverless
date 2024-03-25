f2c6b3b69ddd48d88d2d629e18654d54754531d9
achan80 <chan.albert@gmail.com>
2024-03-25 00:08:53 +0000

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+const getRandomDataJoke = async() => {
+
+  // return "Why shouldn't you wear glasses when you play football.  Because it's a contact sport."
+
+  const url = "https://icanhazdadjoke.com/"
+  const jokeStream = await fetch(url, {
+    headers: {
+      Accept: "application/json"
+    }
+  })
+
+  const jsonJoke = await jokeStream.json();
+  const joke = jsonJoke.joke;
+  return joke;
+}
+
+const displayJoke = (joke) => {
+  const h1 = document.querySelector("h1");
+  h1.textContent = joke;
+}
+
+const refreshJoke = async () => {
+  const joke = await getRandomDataJoke();
+  displayJoke(joke);
+}
+
+//load 1st joke
+refreshJoke();
+
+
+setInterval(refreshJoke, 5000);
+//setInterval(() => {
+//  refreshJoke();
+//}, 3000);
